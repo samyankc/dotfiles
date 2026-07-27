caa29e0daeeb41582efa840db5bbfc54fba6a358
samyankc <80664080+samyankc@users.noreply.github.com>
2026-07-27 21:08:52 +0800

    Update .gitconfig.zsh

--- zsh/.gitconfig.zsh
+++ zsh/.gitconfig.zsh
@@ -1,11 +1,17 @@
 [core]
-    editor = hx
-    fileMode = false
+	editor = hx
+	fileMode = false
 
 [include]
-    path = .gitconfig
-    # path = .config/delta/.gitconfig
-    path = .config/difftastic/.gitconfig
+	path = .gitconfig
+	# path = .config/delta/.gitconfig
+	path = .config/difftastic/.gitconfig
 
 [init]
 	defaultBranch = main
+
+[filter "lfs"]
+	required = true
+	clean = git-lfs clean -- %f
+	smudge = git-lfs smudge -- %f
+	process = git-lfs filter-process
